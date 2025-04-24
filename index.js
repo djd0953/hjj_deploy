98d80317acdefcf3328145da00c224413148fdd8
hjj0106 <hjj0106@amicuslex.net>
2025-04-24 14:05:28 +0900

    main만 빌드 할 수 있게 수정, body의 ref해석을 위해 express사용

--- index.js
+++ index.js
@@ -1,37 +1,41 @@
-const http = require('http');
 const { exec } = require('child_process');
+const express = require('express');
+const app = express();
 
 // 필요한 포트 번호
 const PORT = 9000;
 
-// 간단한 보안용 GitHub Webhook 전용 POST 핸들러
-const server = http.createServer(async (req, res) => {
-  if (req.method === 'POST') {
-
-    console.log(req.parser);
-    console.log('📩 Webhook received at', new Date().toISOString());
-
-    // deploy.sh 실행
-    //exec('/home/hjj0106/hjj-webhook/deploy.sh', (err, stdout, stderr) => {
-      //if (err) {
-        //console.error('❌ Deploy failed:', err);
-        //return res.writeHead(500).end('Deploy failed');
-      //}
-
-      //console.log('✅ Deploy output:\n', stdout);
-      //console.error('⚠️ Deploy stderr:\n', stderr);
-
-      //res.writeHead(200);
-      //res.end('Deploy complete');
-    //});
-  } else {
-    res.writeHead(405); // Method Not Allowed
-    res.end('Not allowed');
-  }
-});
+app.use(express.json())
 
 // 서버 시작
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`🚀 Webhook server listening on port ${PORT}`);
 });
 
+app.post('/', (req, res) => {
+    try {
+      if (!req.body.ref) throw new Error("No ref")
+
+      console.log('📩 Webhook received at', new Date().toISOString());
+      if (req.body.ref === 'refs/heads/main') {
+        // deploy.sh 실행
+        exec('/home/hjj0106/hjj-webhook/deploy.sh', (err, stdout, stderr) => {
+          if (err) {
+            console.error('❌ Deploy failed:', err);
+            return res.writeHead(500).end('Deploy failed');
+          }
+    
+          console.log('✅ Deploy output:\n', stdout);
+          console.error('⚠️ Deploy stderr:\n', stderr);
+    
+          res.writeHead(200);
+          res.end('Deploy complete');
+        });
+      }
+    }
+    catch (error) {
+      console.error('❌ Deploy failed:', err);
+      res.writeHead(405); // Method Not Allowed
+      res.end('Not allowed');
+    }
+})
